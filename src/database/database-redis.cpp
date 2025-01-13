f7545cbc12c7a05c4a5abd165ac3712bb017f37f
kawogi <github@kai.giebeler.de>
2025-01-13 01:46:51 +0100

    remove client code

--- src/database/database-redis.cpp
+++ src/database/database-redis.cpp
@@ -11,7 +11,7 @@
 #include "settings.h"
 #include "log.h"
 #include "exceptions.h"
-#include "irrlicht_changes/printing.h"
+#include "irrlicht_changes/	"
 #include "util/string.h"
 
 #include <hiredis.h>
